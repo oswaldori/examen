8624fe37a3a85b906fa0d3c9c201d933c9143f93
oswaldori <96199469+oswaldori@users.noreply.github.com>
2022-01-26 15:47:58 -0600

    Se agrega el proyecto

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using ExamenWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace ExamenWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ObtenerDatos(DateTime fechaReporte, DateTime fechaSiniestro)
+        {
+            using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["Examen"].ConnectionString))
+            {
+
+                DataTable dt = new DataTable();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "Consulta";
+                cmd.Parameters.Add("@svfhreporte", SqlDbType.DateTime).Value = fechaReporte;
+                cmd.Parameters.Add("@svfhsiniestro", SqlDbType.DateTime).Value = fechaSiniestro;
+                cmd.Connection = con;
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                con.Close();
+
+                List<MDatos> objetoListaMDatos = new List<MDatos>();
+                foreach (DataRow Dr in dt.Rows)
+                {
+                    MDatos ListaMDatos = new MDatos();
+                    ListaMDatos.nrreporte = Convert.ToInt32(Dr["nrreporte"]);
+                    ListaMDatos.svindice = Convert.ToInt32(Dr["svindice"]);
+                    ListaMDatos.svfhreporte = Convert.ToDateTime(Dr["svfhreporte"]);
+                    ListaMDatos.clnombre = Convert.ToString(Dr["clnombre"]);
+                    ListaMDatos.clestatus = Convert.ToString(Dr["clestatus"]);
+                    ListaMDatos.prrfc = Convert.ToString(Dr["prrfc"]);
+                    ListaMDatos.sunombre = Convert.ToString(Dr["sunombre"]);
+                    ListaMDatos.susupervisoria = Convert.ToString(Dr["susupervisoria"]);
+
+                    objetoListaMDatos.Add(ListaMDatos);
+                }
+                return Json(objetoListaMDatos, JsonRequestBehavior.AllowGet);
+
+            }
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Models/MDatos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ExamenWeb.Models
+{
+    public class MDatos
+    {
+        public int nrreporte { get; set; }
+        public int svindice { get; set; }
+        public DateTime svfhreporte { get; set; }
+        public string clnombre { get; set; }
+        public string clestatus { get; set; }
+        public string prnombre { get; set; }
+        public string prrfc { get; set; }
+        public string sunombre { get; set; }
+        public string susupervisoria { get; set; }
+    
+}
+}
